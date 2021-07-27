21c6eecee1f4c325f5cc400a8448787250be2c9f
wszgrcy <wszgrcy@gmail.com>
2021-07-27 23:08:00 +0800

    test: 装饰器 Optional

--- test/fixture/hello-world.ts
+++ test/fixture/hello-world.ts
@@ -3,19 +3,22 @@ import {
   R3Injector,
   InjectionToken,
   Inject,
+  Optional,
 } from 'static-injector';
+import { NullInjector } from '../../src/import/di/null_injector';
 
 @Injectable()
 export class FirstClass {
-  constructor(@Inject(token) token) {
+  constructor(@Inject(token) token, @Inject('noValue') @Optional() noValue) {
     console.log('输出', token);
+    console.log('noValue', noValue);
   }
 }
 let token = new InjectionToken('');
 let injector = new R3Injector(
   { name: 'test' } as any,
   [{ provide: FirstClass }, { provide: token, useValue: 123 }],
-  undefined
+  new NullInjector()
 );
 let instance = injector.get(FirstClass);
 console.log(instance);
