dd36e43f52bae09dc8f031eefb70cc3f48d53fd2
chen <wszgrcy@gmail.com>
2024-03-23 12:16:11 +0800

    update 17.2.4 去掉了signal相关,未来如果需要再加回来,不过如果真的需要那么还需要改更多

--- script/sync/index.ts
+++ script/sync/index.ts
@@ -53,7 +53,7 @@ let fn: ScriptFunction = async (util, rule, host, injector) => {
     ],
     'packages',
     'branch',
-    '17.2.0'
+    '17.2.4'
   );
   let copyData = require('./copy.json') as {
     source: string;
