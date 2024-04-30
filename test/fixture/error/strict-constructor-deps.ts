6454987286be87dd16812da0333b24e7b30631a0
chen <wszgrcy@gmail.com>
2024-04-30 09:41:24 +0800

    test: error

--- test/fixture/error/strict-constructor-deps.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, Injector } from 'static-injector';
-
-export class MyClass {
-  constructor(a) {}
-}
-let injector = Injector.create({ providers: [{ provide: MyClass }] });
-export const instance = injector.get(MyClass);
--- test/fixture/error/strict-constructor-interface-type-dep.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, Injector } from 'static-injector';
-interface A {}
-
-export class MyClass {
-  constructor(a: A) {}
-}
-let injector = Injector.create({ providers: [{ provide: MyClass }] });
-export const instance = injector.get(MyClass);
