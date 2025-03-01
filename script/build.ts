643075f4a61958aed167ee246b3215f084b74abb
chen yang <wszgrcy@gmail.com>
2025-03-02 01:18:02 +0800

    构建

--- script/build.ts
+++ script/build.ts
@@ -1,15 +1,33 @@
-import * as rollup from 'rollup';
-
+import * as esbuild from 'esbuild';
+import * as path from 'path';
+import * as glob from 'fast-glob';
 async function bundleImport() {
-  let bundle = await rollup.rollup({
-    input: './dist/es2022/index.js',
-    treeshake: true,
-  });
-
-  await bundle.write({ file: './dist/commonjs/index.js', format: 'commonjs' });
-  await bundle.write({
-    file: './dist/fesm2022/index.js',
-    format: 'module',
+  let options: esbuild.BuildOptions = {
+    platform: 'node',
+    sourcemap: 'linked',
+    bundle: true,
+    entryPoints: [{ in: './src/import/index.ts', out: './index' }],
+    splitting: false,
+    outdir: path.join(process.cwd(), './test-dist'),
+    outExtension: {
+      '.js': '.mjs',
+    },
+    format: 'esm',
+    minify: true,
+    tsconfig: 'tsconfig.import.json',
+    charset: 'utf8',
+    external: ['rxjs', 'mocha', 'chai'],
+    define: {
+      ngDevMode: 'false',
+      Zone: 'undefined',
+    },
+  };
+  await esbuild.build({ ...options, outdir: './dist' });
+  await esbuild.build({
+    ...options,
+    outdir: './dist',
+    format: 'cjs',
+    outExtension: {},
   });
 }
 
